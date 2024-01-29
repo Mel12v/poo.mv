bb96790c2ac5132afee36983d9f6ef8c2791d369
Lab A2 <mvillalba1@est.ups.edu.ec>
2024-01-29 13:11:40 -0500

    Add(II): Add PrintTrinagle Function

--- poo/src/Main.java
+++ poo/src/Main.java
@@ -3,6 +3,11 @@ public class Main {
         printSquare(10);
         System.out.println("---------------------");
         printSquare(20);
+        System.out.println("---------------------");
+        printTriangle(6);
+        System.out.println("---------------------");
+        printTriangle(4);
+
 
     }
 
@@ -16,6 +21,15 @@ public class Main {
             }
             System.out.print("\n");
 
+        }
+    }
+
+    public static void printTriangle(int base) {
+        for (int i = 1; i <= base; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.print("\n");
         }
     }
 }
