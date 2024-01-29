f347b339ae310c697fe539e776bfb2ebdae8e4bb
Lab A2 <mvillalba1@est.ups.edu.ec>
2024-01-29 11:32:35 -0500

    Add(II): Add PrintSquare Fucntion

--- poo/src/Main.java
+++ poo/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printSquare(10);
+        System.out.println("---------------------");
+        printSquare(20);
+
+    }
+
+    public static void printSquare(int git glength) {
+        //for that prints width
+        for (int i = 1; i <= length; i++) {
+            //for that prints height
+            for (int j = 1; j <= length; j++) {
+                System.out.print(" * ");
+
+            }
+            System.out.print("\n");
+
+        }
     }
 }
